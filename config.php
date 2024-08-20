7ffaec1a2dc669571bf9ea7ba561d7f0e128b26d
Nganthoiba <leecba@gmail.com>
2024-08-20 16:38:32 +0530

    modified and corrected

--- config.php
+++ config.php
@@ -6,22 +6,22 @@ $config = [
 ];
 
 //$config = [];
+if(isset($argv)){
+    //however if argument is supplied at command line
+    $sizeOfArgv = sizeof($argv);
+    if($sizeOfArgv > 1){
+        for($i = 1; $i < $sizeOfArgv; $i++){
+            $argument = explode('=', $argv[$i]);
+            $key = $argument[0];
+            $value = $argument[1];
 
-//however if argument is supplied at command line
-$sizeOfArgv = sizeof($argv);
-if($sizeOfArgv > 1){
-    for($i = 1; $i < $sizeOfArgv; $i++){
-        $argument = explode('=', $argv[$i]);
-        $key = $argument[0];
-        $value = $argument[1];
+            /* $config[] = [
+                $argument[0] => $argument[1]
+            ]; */
 
-        /* $config[] = [
-            $argument[0] => $argument[1]
-        ]; */
-
-        $config[$key] = $value;
+            $config[$key] = $value;
+        }
     }
 }
-
 define('HOST_NAME',$config['--host']); 
 define('PORT',$config['--port']);
